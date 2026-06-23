e66738353f356a09310e41e15f068e7eb53ef876
gendolslebew-lgtm <gendolslebew@gmail.com>
2026-06-23 13:54:29 +0700

    Fix paksa HTTPS sudah disave

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,7 +18,9 @@ class AppServiceProvider extends ServiceProvider
      * Bootstrap any application services.
      */
     public function boot(): void
-    {
-        //
+    { 
+     if (config('app.env') === 'production' || env('APP_ENV') === 'local') {
+    \URL::forceScheme('https');
+}   //
     }
 }
